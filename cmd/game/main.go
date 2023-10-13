8a0f4cb6fab6080cab70e6aee60f5e8da65c4829
Iskander Sharipov <quasilyte@gmail.com>
2023-10-13 21:41:09 +0400

    add a simple game stub

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"time"
+
+	"github.com/quasilyte/ge"
+	"github.com/quasilyte/vcgj7-game/scenes"
+)
+
 func main() {
-	// TODO
+	ctx := ge.NewContext(ge.ContextConfig{})
+	ctx.Rand.SetSeed(time.Now().Unix())
+	ctx.GameName = "planet_eaters"
+	ctx.WindowTitle = "Planet Eaters"
+	ctx.WindowWidth = 1920 / 2
+	ctx.WindowHeight = 1080 / 2
+	ctx.FullScreen = true
+
+	if err := ge.RunGame(ctx, scenes.NewMainMenuController()); err != nil {
+		panic(err)
+	}
 }
